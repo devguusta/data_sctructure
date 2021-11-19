4836c08dcfe1e7eede5098afafb99aeefd399425
Gustavo Silva Candido Cabral <gustavo.cabral@rarolabs.com.br>
2021-11-19 18:20:33 -0300

    busca binaria

---
+++ binary_search.dart
@@ -0,0 +1,82 @@
+void main() {
+  List<int> list = [
+    0,
+    1,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    41,
+    42,
+    35,
+    26,
+    131,
+    92,
+    123,
+    90,
+  ];
+  int userValue = list[5];
+  int max = list.length - 1;
+  int min = 0;
+
+  binarySearch(userValue: userValue, min: min, max: max, list: list);
+  binary(list: list, item: 123);
+}
+
+binary({required List<int> list, required int item}) {
+  int min = 0;
+  int max = list.length - 1;
+
+  while (min <= max) {
+    int mid = ((max + min) / 2).round();
+    int value = list[mid];
+    if (value == item) {
+      print("mid $mid");
+
+      print("O valor é $value na posição $item");
+    }
+    if (value > item) {
+      max = mid - 1;
+      print("o item  é $item");
+    } else {
+      min = mid + 1;
+      print(" o item  é esse : $item");
+    }
+  }
+}
+
+binarySearch(
+    {required List<int> list,
+    required int userValue,
+    required int min,
+    required int max}) {
+  if (max >= min) {
+    print('min $min');
+    print('max $max');
+    int mid = ((max + min) / 2).floor();
+    if (userValue == list[mid]) {
+      print('your item is at index: ${mid}');
+    } else if (userValue > list[mid]) {
+      binarySearch(list: list, userValue: userValue, min: mid + 1, max: max);
+    } else {
+      binarySearch(list: list, userValue: userValue, min: min, max: mid - 1);
+    }
+  }
+  return null;
+}
